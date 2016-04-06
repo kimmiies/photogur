e88a301fb78976392a06064840df9652e24c11ff
Kimberly Oleiro <koleiro@gmail.com>
2016-04-06 16:07:58 -0400

    Refactoring to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,11 +7,27 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
-  def new
+  def new #going to update the view with this new method
+    @picture = Picture.new
   end
 
   def create
-    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+#makes a new picture with what picture_params returns
+#this is the method we're calling (post pictures => pictures#create)
+    @picture = Picture.new(picture_params) #picture info that came in the request
+    if @picture.save #if the picture was successfully saved redirect
+      redirect_to pictures_url
+    else
+      render :new #Render the view associated with the action :new (new.html.erb)
+    end
+    #render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
   end
+
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
 
 end
